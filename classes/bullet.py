6ff22754f0f0b3c2f17935b93b336064002ad572
KiRtAp2 <posta.patrik@gmail.com>
2017-12-05 17:57:28 +0100

    Malo izboljsal pozicioniranje metkov, ko se pojavijo

--- classes/bullet.py
+++ classes/bullet.py
@@ -1,27 +1,54 @@
+from math import tan, radians
 import consts
 from classes import moving_object
 from utility.texture_loader import textures
 
 
+def get_position(x, y, rot, sx, sy):
+    """Return position to spawn bullet"""
+    xpos = x
+    ypos = y
+
+    if rot < 0:
+        rot += 360
+    if rot > 360:
+        rot %= 360
+
+    # y
+    opp = tan(radians(rot)) / sy
+    if rot < 45:
+        pass
+    elif rot < 90:
+        ypos += (sy / 2 - opp)
+    elif rot < 135:
+        ypos += sy / 2 + opp
+    elif rot < 225:
+        ypos += sy
+    elif rot < 270:
+        ypos += sy/2 - opp
+
+    # x
+    if rot < 45:
+        xpos += sx/2 - opp
+    elif rot < 135:
+        pass
+    elif rot < 180:
+        xpos += sx/2 - opp
+    elif rot < 225:
+        xpos += sx/2 + opp
+    elif rot < 315:
+        xpos += sx
+    else:
+        xpos += sx/2 + opp
+
+    return xpos, ypos
+
+
 class Bullet(moving_object.MovingObject):
 
     def __init__(self, player):
-
-        # calculates how much to the right the bullet should spawn
-        if player.rotation > 180:
-            r = player.rotation-90
-        else:
-            r = player.rotation+90
-        if r > 360:
-            r -= 360
-        if r > 135:
-            k = 1
-        elif r < 45:
-            k = 0
-        else:
-            k = (r-45)/90
-        k = 1 - k
-        xadd = k*player.sx
-        super(Bullet, self).__init__((player.x+xadd, player.y), consts.BULLET_SIZE, textures["bullet"])
+        self.rotation = player.rotation + 180
+        xpos, ypos = get_position(player.x, player.y, self.rotation-180, *consts.PLAYER_SIZE)
+        super(Bullet, self).__init__((xpos, ypos), consts.BULLET_SIZE, textures["bullet"])
         self.velocity = consts.BULLET_SPEED
-        self.rotation = player.rotation+180
+        self.rotation = player.rotation + 180
