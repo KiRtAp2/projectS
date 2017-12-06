24cb8932d0c2cfb7bdfe2e96ff669493d3964cf5
KiRtAp2 <posta.patrik@gmail.com>
2017-12-06 11:24:09 +0100

    Preprecil, da se nasprotniki pojavijo v totemu (upam)

--- classes/enemy.py
+++ classes/enemy.py
@@ -1,6 +1,6 @@
 import consts
 from classes import moving_object
-from utility.randomgen import rand_pos
+from utility.randomgen import rand_pos_nocenter
 from consts import ENEMY_SIZE as SIZE
 from utility.texture_loader import textures
 import math
@@ -9,7 +9,7 @@ import math
 class Enemy(moving_object.MovingObject):
 
     def __init__(self, totem_pos, curr_score):
-        super(Enemy, self).__init__(rand_pos(*SIZE), SIZE, textures["enemy"])
+        super(Enemy, self).__init__(rand_pos_nocenter(SIZE, totem_pos, consts.TOTEM_SIZE[0]), SIZE, textures["enemy"])
         self.velocity = -int(consts.START_ENEMY_VELOCITY+curr_score*0.15)
         try:
             opp = -self.x - self.sx/2 + totem_pos[0]
--- utility/randomgen.py
+++ utility/randomgen.py
@@ -2,6 +2,22 @@ import random
 import consts
 
 
+def _check_center_ok(centerpos, centersize, pos):
+	"""Checks whether pos is far away enough from centerpos"""
+	x, y = False, False
+	if pos[0]-centersize > centerpos[0] or pos[0]+centersize < centerpos[0]: x = True
+	if pos[1]-centersize > centerpos[1] or pos[0]+centersize < centerpos[1]: y = True
+	return (x and y)
+
+
 def rand_pos(sx=0, sy=0):
     """Return tuple of random position but not so high that object goes over bounds (size provided by sx and sy)"""
     return random.randrange(0, consts.WWIDTH-sx), random.randrange(0, consts.WHEIGHT-sy)
+
+
+def rand_pos_nocenter(size, centerpos, centersize):
+	"""Return random position but not centersize out of centerpos"""
+	pos = rand_pos()
+	while not _check_center_ok(centerpos, centersize, pos):
+		pos = rand_pos()
+	return pos
